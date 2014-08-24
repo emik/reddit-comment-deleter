b5116ffc68e25ee91664518a5759580d8969c788
emik <yoakydo@gmail.com>
2014-08-25 00:41:53 +0100

    modifies setup.bat slightly

--- Setup.bat
+++ Setup.bat
@@ -2,6 +2,6 @@
 set /p username=Enter username:%=%
 set /p password=Enter password:%=%
 set /p deleteDistanceDays=Enter the number of days in the past before which you wish to delete reddit comments:%=%
-@echo username=%username% > config.ini
-@echo password=%password% >> config.ini
-@echo deleteDistanceDays=%deleteDistanceDays% >> config.ini
+@echo username = %username% > config.ini
+@echo password = %password% >> config.ini
+@echo deleteDistanceDays = %deleteDistanceDays% >> config.ini
