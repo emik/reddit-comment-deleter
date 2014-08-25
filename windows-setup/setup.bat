4a5d78eed44f00031aa49da820512bdb906382ac
emik <yoakydo@gmail.com>
2014-08-25 01:06:59 +0100

    adds windows based execution file and moves to own folder

---
+++ windows-setup/setup.bat
@@ -0,0 +1,7 @@
+@echo off
+set /p username=Enter username:%=%
+set /p password=Enter password:%=%
+set /p deleteDistanceDays=Enter the number of days in the past before which you wish to delete reddit comments:%=%
+@echo username = %username% > config.ini
+@echo password = %password% >> config.ini
+@echo deleteDistanceDays = %deleteDistanceDays% >> config.ini
